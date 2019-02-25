7adde791c8a31283a20b5a53271d1da7d2353b65
frnkdtnk <fpineda@mit.edu>
2019-02-25 02:14:59 -0500

    frankie added

--- exercise.js
+++ exercise.js
@@ -1,56 +1,46 @@
 var exercise = {};
 
 exercise.countRecords = function(data){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the total number of records
-    //-------------------------------------------
-    return 'Error: countRecords not implemented'; 
+    return data.length;
 };
 
 exercise.countDistrictCrimes = function(data,district){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given district
-    //-------------------------------------------
-    return 'Error: countDistrictCrimes not implemented'; 
-};
-
-exercise.countPrimaryType = function(data,primaryType){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the giving primary type
-    //-------------------------------------------
-    return 'Error: countPrimaryType not implemented';
-};
-
-exercise.countLocation = function(data,location){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given location
-    //-------------------------------------------
-    return 'Error: countLocation not implemented';
-};
-
-
-exercise.buildLatLngPoint = function(crime){
-    //-------------------------------------------
-    // YOUR CODE
-    // build the point for the given crime
-    // 
-    // For example:
-    //
-    // var point = {};    
-    // point.latitude = crime[latitudeColumnNumber];
-    // point.longitude = crime[longitudeColumnNumber];
-    // return point;    
-    //-------------------------------------------    
-    return 'Error: buildLatLngPoint not implemented';
-};
-
-
-
+    number=0;
+    function add(item,counter,array){
+        if (item[19]==district){number+=1};
+    }
+    data.forEach(add);
+     return number; 
+ };
+ 
+ exercise.countPrimaryType = function(data,primaryType){
+    number=0;
+    function add(item,counter,array){
+        if (item[13]==primaryType){number+=1};
+    }
+    data.forEach(add);
+     return number;
+ };
+ 
+ exercise.countLocation = function(data,location){
+     number=0;
+    function add(item,counter,array){
+        if (item[15]==location){number+=1};
+    }
+    data.forEach(add);
+     return number;
+ };
+ 
+ 
+ exercise.buildLatLngPoint = function(crime){
+     
+     var point = {};    
+     point.latitude = crime[26];
+     point.longitude = crime[28];
+     return point;    
+     
+       
+     
+ };
+ 
 
